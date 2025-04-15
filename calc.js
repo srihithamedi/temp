3cc7bffb2affc58ae1f77b2240e6567ab87ff027
srihithamedi <medisrihitha05@gmail.com>
2025-04-15 12:05:37 +0530

    commit

---
+++ calc.js
@@ -0,0 +1,8 @@
+function greet() {
+    return "Hello";
+  }
+  function add(a, b) {
+    return a + b;
+  }
+  export { add };
+  export default greet
---
+++ main.js
@@ -0,0 +1,6 @@
+import greet from "./calc.js"
+import {add} from "./calc.js"
+let result = greet()
+console.log(result)
+// let result = add(4,5)
+// console.log(result)
